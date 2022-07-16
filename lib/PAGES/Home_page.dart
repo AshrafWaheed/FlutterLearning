9dadff238e652d7509fba4582c246ecf881e1f29
Ashraf Waheed <ashraframnagar@gmail.com>
2022-07-16 12:16:50 +0530

    dy 3

--- lib/PAGES/Home_page.dart
+++ lib/PAGES/Home_page.dart
@@ -1,5 +1,13 @@
+// ignore: duplicate_ignore
+
+
+
+
+// ignore_for_file: use_key_in_widget_constructors, file_names, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
   final int days = 30;
   var suck = "Fuckers";
@@ -14,6 +22,7 @@ class HomePage extends StatelessWidget {
         title: Text("fuck yeah") ,
       ),
       body:Center(
+        // ignore: avoid_unnecessary_containers
         child: Container(
           child: Text("welcome $days days $suck"),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_app_1/login_page.dart';
 
-import 'Home_page.dart';
+import 'PAGES/Home_page.dart';
 
  void main(List<String> args) {
-  runApp(myApp());
+  runApp(const myApp());
 
 }
 
+// ignore: camel_case_types
 class myApp extends StatelessWidget {
   const myApp({Key? key}) : super(key: key);
 
@@ -17,7 +18,15 @@ class myApp extends StatelessWidget {
     
 
     return MaterialApp(
-      home: HomePage(),
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      routes: {
+        "/": (context)=> HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
